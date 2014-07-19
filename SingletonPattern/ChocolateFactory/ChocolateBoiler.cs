ad22c1194dd30c7adff2ddb97c1df339d86e1f81
tenevdev <tenev.dev@outlook.com>
2014-07-19 15:29:17 +0300

    Create singleton

---
+++ SingletonPattern/ChocolateFactory/ChocolateBoiler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern.ChocolateFactory
+{
+    public sealed class ChocolateBoiler
+    {
+        private static readonly ChocolateBoiler _instance = new ChocolateBoiler();
+        public static ChocolateBoiler Instance 
+        {
+            get 
+            {
+                return ChocolateBoiler._instance;
+            }
+        }
+
+        private ChocolateBoiler()
+        {
+        }
+    }
+}
